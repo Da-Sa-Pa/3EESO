1c873407ad050dbfcfe46001366b04d1513e80c7
Daniel <109203259+Da-Sa-Pa@users.noreply.github.com>
2022-10-02 15:41:35 +0200

    js

--- Javascript.js
+++ Javascript.js
@@ -28,11 +28,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 8000); // Change image every 2 seconds
 }
