779952084c84653000a55a6a3d6fbfdce433721a
Daniel <109203259+Da-Sa-Pa@users.noreply.github.com>
2022-10-02 14:19:38 +0200

    trial

--- Javascript.js
+++ Javascript.js
@@ -28,13 +28,6 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-}
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
